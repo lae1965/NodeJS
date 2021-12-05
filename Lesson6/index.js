82d87f99ec412c92bc0de5208256020bdc59779b
lae1965 <lae1965@yandex.ru>
2021-12-05 19:40:09 +0300

    Сделал домашнее задание к уроку №6

--- Lesson6/index.js
+++ Lesson6/index.js
@@ -1,13 +1,28 @@
-const fs  = require('fs');
-const http  = require('http');
-const path  = require('path');
-const socket  = require('socket.io');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const socket = require('socket.io');
 
 class Connection {
     constructor(id, clientName) {
+        this.id = id,
+        this.clientName = clientName,
+        this.connect = true
+    }
+    get getConnect() {
+        return this.connect;
+    }
+    get getClientName() {
+        return this.clientName;
+    }
+    get getId() {
+        return this.id;
+    }
+    set setConnect(connect) {
+        this.connect = connect;
+    }
+    set setId(id) {
         this.id = id;
-        this.clientName = clientName;
-        this.connect = true;
     }
 };
 
@@ -22,33 +37,47 @@ const server = http.createServer((req, res) => {
 
 const io = socket(server);
 io.on('connection', (client) => {
-    io.emit('server-msg', {
-        message: `A new client ${client.id} has connected`
-    });
 
-    client.on('disconnect', () =>{
-        io.emit('server-msg', {
-            message: `The client ${client.id} has disconnected`
-        });
+    client.on('disconnect', () => {
+        const find = connectionsList.findIndex(connection => connection.getId === client.id);
+        if (find !== -1) {
+            connectionsList[find].setConnect = false;
+            io.emit('server-msg', {
+                clientName: 'Server',
+                message: `The client ${connectionsList[find].getClientName} has disconnected`
+            });
+        }
+    });
+ 
+    client.on('client-name', clientName => {
+        const find = connectionsList.findIndex(connection => connection.getClientName === clientName);
+        if (find === -1) {
+            connectionsList.push(new Connection(client.id, clientName));
+            client.broadcast.emit('server-msg', {
+                clientName: 'Server',
+                message: `A new client ${clientName} has connected`
+            });
+        } else {
+            if (connectionsList[find].getConnect) {
+                client.emit('name-already-exist');
+            } else {
+                connectionsList[find].setConnect = true;
+                connectionsList[find].setId = client.id;
+                client.broadcast.emit('server-msg', {
+                    clientName: 'Server',
+                    message: `The client ${clientName} has reconnected`
+                });
+            }
+        }
     });
 
     client.on('client-msg', (data) => {
         const payload = {
+            clientName: (connectionsList.find(connection => 
+                connection.getId === client.id)).getClientName,
             message: data.message.split('').reverse().join('')
         };
         client.broadcast.emit('server-msg', payload);
-        client.emit('server-msg', payload);
-    });
-
-    client.on('client-name', (clientName) => {
-        const find = connectionsList.findIndex((connection) => {
-            connection.clientName === clientName;
-        });
-        if (find === -1) {
-            connectionsList.push(new Connection(client.id, clientName));
-        } else {
-            if (connectionsList[find].connect) 
-        }
     });
 });
 
